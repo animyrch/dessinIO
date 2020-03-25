f078633e909accb260660767cad49b483c2a7b54
jumperdenfer <unconfigured@null.spigotmc.org>
2020-03-25 16:31:38 +0100

    Utilisation de documentFragment pour éviter le reflow

--- src/js/domManager.js
+++ src/js/domManager.js
@@ -1,11 +1,14 @@
 const updateUserMessages = (userMessages) => {
     chat.innerHTML = '';
+    //Créé un fragment de document qui vas contenir les node message avant leurs insertion
+    let messagesFragment = document.createDocumentFragment();
     for(const message of userMessages) {
         var node = document.createElement("DIV");
         var textnode = document.createTextNode(message);
         node.appendChild(textnode);
-        chat.appendChild(node);
+        messagesFragment.appendChild(node);
     }
+    chat.appendChild(messageFragment);
 }
 
 export default {
